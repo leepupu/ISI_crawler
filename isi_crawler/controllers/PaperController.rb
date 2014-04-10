f92a222f94ca610a813266edb0efab0c59a8534e
LeePu(NB) <leepupu@gmail.com>
2014-04-10 20:42:42 +0800

    batch sample

--- isi_crawler/controllers/PaperController.rb
+++ isi_crawler/controllers/PaperController.rb
@@ -1,12 +1,10 @@
 class PaperController
   def initialize
-    agent = Mechanize.new
-    search_page = agent.get('http://isiknowledge.com/?DestApp=WOS')
+    @agent = Mechanize.new
+    search_page = @agent.get('http://isiknowledge.com/?DestApp=WOS')
     @search_form = search_page.form_with(:name => 'WOS_GeneralSearch_input_form')
     @search_form['value(select1)'] = 'AD'
-    form['value(input1)'] = 'Natl Cent Univ'
-    
-    # //*[@id="hitCount.top"]
+    @search_form['value(input1)'] = 'Natl Cent Univ'
   end
 
   def run(s_year=2014,e_year=2014)
@@ -18,16 +16,16 @@ class PaperController
     @search_form['startYear'] = year
     @search_form['endYear'] = year
     result = @search_form.submit
-    total = result.search('//*[@id="footer_formatted_count"]').text.gsub!(',','').to_i
+    total = result.search('//*[@id="footer_formatted_count"]').text.gsub(',','').to_i
     uri = Addressable::URI.parse(result.search('//*[@id="RECORD_1"]/div[3]/div[1]/div/a')[0]['href'])
-    params = uri.query_params
     total.times do |i|
-      uri.query_params['doc'] = i+1
+      q = uri.query_values
+      q['doc'] = i+1
+      uri.query_values = q
       puts uri.query
-      paper = agent.get('http://isiknowledge.com/full_record.do?'+uri.query)
-      puts paper.search('//*[@id="records_form"]/div/div/div/div[1]/div/div[1]/value').text
+      paper = @agent.get('http://apps.webofknowledge.com/full_record.do?'+uri.query)
+      puts paper.search('//*[@class="title"]').text
     end
-    
   end
 
   def build(html)
