15116d60fd4a0d121258a1dfb96a6343379bd6d3
LeePu(NB) <leepupu@gmail.com>
2014-04-10 16:50:05 +0800

    imply some controller method

--- isi_crawler.rb
+++ isi_crawler.rb
@@ -2,11 +2,13 @@ require 'mechanize'
 require 'mongo_mapper'
 require 'yaml'
 require 'abstract'
+require 'date'
 
 module ISICrawler
 
   Dir["./isi_crawler/models/*.rb"].each {|file| require file }
   Dir["./isi_crawler/models/parser/*.rb"].each {|file| require file }
+  Dir["./isi_crawler/controllers/*.rb"].each {|file| require file }
 
   module_function
   def search_form
@@ -28,6 +30,10 @@ module ISICrawler
 
   def init
     Dir["./config/initializers/*.rb"].each {|file| require file }
+  end
+
+  def test
+    init
   end
 
 end
--- isi_crawler/controllers/PaperController.rb
+++ isi_crawler/controllers/PaperController.rb
@@ -1,25 +1,38 @@
-require 'mechanize'
-require 'mongo_mapper'
-require 'yaml'
-require 'date'
-
 class PaperController
   def initialize
     agent = Mechanize.new
     search_page = agent.get('http://isiknowledge.com/?DestApp=WOS')
     @search_form = search_page.form_with(:name => 'WOS_GeneralSearch_input_form')
+    @search_form['value(select1)'] = 'AD'
+    form['value(input1)'] = 'Natl Cent Univ'
+    
+    # //*[@id="hitCount.top"]
   end
 
-  def run
+  def run(s_year=2014,e_year=2014)
     #TODO: slice time, multi thread?, batch data
   end
 
-  def batch(start_date, end_date)
+  def batch(year)
     #TODO: grap data in date range by build
+    @search_form['startYear'] = year
+    @search_form['endYear'] = year
+    result = @search_form.submit
+    total = result.search('//*[@id="footer_formatted_count"]').text.gsub!(',','').to_i
+    uri = Addressable::URI.parse(result.search('//*[@id="RECORD_1"]/div[3]/div[1]/div/a')[0]['href'])
+    params = uri.query_params
+    total.times do |i|
+      uri.query_params['doc'] = i+1
+      puts uri.query
+      paper = agent.get('http://isiknowledge.com/full_record.do?'+uri.query)
+      puts paper.search('//*[@id="records_form"]/div/div/div/div[1]/div/div[1]/value').text
+    end
+    
   end
 
   def build(html)
-    #TODO: build Paper by html
+    #TODO: build Paper by html, html is search result page
+    # => build Paper with paper.show page's html
   end
 
 end
